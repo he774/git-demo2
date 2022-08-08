d981e6e940c3f04464e40b27f03e5a58ce56df06
he <he@qq.com>
2022-08-08 16:19:51 +0800

    idea second commit

---
+++ src/main/java/com/itheima/boot_demo/BootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("git second");
+        SpringApplication.run(BootDemoApplication.class, args);
+    }
+
+}
